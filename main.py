aaca3191a8cc92d01dc5b1fb1e029ea58d493e62
dolpxx <s12102202190@toyo.jp>
2023-03-02 03:10:54 +0900

    added get_value()

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import json
 
+
 ponya = {"Atomaru": "C", "Kanbara": "AA",
-         "Hamuko": "E", "Konoha": "B", "Mario": "F","gamera": "G"}
+         "Hamuko": "E", "Konoha": "B", 
+         "Mario": "F", "Gamera": "G", "oppai": {"size": "chiisai", "cup": 26}}
+
+def get_value(table: dict, *args: str):
+    assert (type(table) == dict)
+    result = table
+    for key in args:
+        result = result[key]
+
+    return (result)
 
 
-with open(f"data/sample.json", mode="w") as f:
-    json.dump(ponya, f, indent=4)
+def edit_value(table: dict, *args: str) -> None:
+    assert (type(table) == dict)
+    result = table
+    for key in args:
+        result = result[key]
+
+    return (result)
+
+# with open(f"data/sample.json", mode="w") as f:
+#     json.dump(ponya, f, indent=4)

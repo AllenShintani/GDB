58b3470479fe4f6936ce0f7cac4271445c08e602
dolpxx <s12102202190@toyo.jp>
2023-03-02 21:59:18 +0900

    add some function()

--- main.py
+++ main.py
@@ -1,26 +1,69 @@
+import os
+import git
 import json
+from box import Box
+from typing import Any
+
+hashable = int | str | tuple | frozenset
+
+ponya: dict = {"Atomaru": "C", "Kanbara": "AA",
+               "Hamuko": "E", "Konoha": "B",
+               "Mario": "F", "Gamera": "G",
+               "oppai": {"A": 1, "B": 2, "C": {"D": 42}}
+               }
 
 
-ponya = {"Atomaru": "C", "Kanbara": "AA",
-         "Hamuko": "E", "Konoha": "B", 
-         "Mario": "F", "Gamera": "G", "oppai": {"size": "chiisai", "cup": 26}}
+def json_to_dict(path: str) -> dict:
+    with open(path) as f:
+        df: dict = json.load(f)
+    return (df)
 
-def get_value(table: dict, *args: str):
-    assert (type(table) == dict)
-    result = table
+
+def get_value(path: str, *args: hashable) -> Any:
+
+    df: dict = json_to_dict(path)
+
+    mybox: Box = Box(df, box_dots=True)
+    keys: list = list()
     for key in args:
-        result = result[key]
+        keys.append(key)
 
-    return (result)
+    KEY: str = ".".join(keys)
+
+    if (KEY == ''):
+        return (df)
+    return (mybox[KEY])
 
 
-def edit_value(table: dict, *args: str) -> None:
-    assert (type(table) == dict)
-    result = table
-    for key in args:
-        result = result[key]
+def edit_value(table: dict, *args: hashable) -> None:
 
-    return (result)
+    key_count: int = len(args)
+    table_copy: dict = table.copy()
 
-# with open(f"data/sample.json", mode="w") as f:
-#     json.dump(ponya, f, indent=4)
+    for i in range(key_count - 1):
+        table = {}
+
+
+def add_value(path: str, *args: dict) -> None:
+    dict_is_empty: bool
+
+    if (len(args) == 0):
+        dict_is_empty = True
+    if (len(args) == 1):
+        dict_is_empty = False
+    if (len(args) >= 2):
+        raise TypeError(
+            "add_value() takes 0 or 1 positional arguments but more arguments were given")
+
+    assert (type(args[0]) == dict)
+
+    with open(path, mode='w') as f:
+        if (dict_is_empty == True):
+            f.write(json.dumps(dict(), indent=4))
+        if (dict_is_empty == False):
+            f.write(json.dumps(args[0], indent=4))
+
+
+def make_branch(name: str) -> None:
+    with open(f"ponya/{name}.json", mode='w') as f:
+        f.write(json.dumps(dict(), indent=4))

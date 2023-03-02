212128c0c39f286af5505a08436f17b8060b1ad4
dolpxx <s12102202190@toyo.jp>
2023-03-03 00:28:19 +0900

    add minimum components

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
+import git
 import json
 from box import Box
 from typing import Any
 
-hashable = int | str | tuple | frozenset
+keyable = str | int | float | bool | None
 
 ponya: dict = {"Atomaru": "C", "Kanbara": "AA",
                "Hamuko": "E", "Konoha": "B",
@@ -11,15 +13,16 @@ ponya: dict = {"Atomaru": "C", "Kanbara": "AA",
                }
 
 
-def json_to_dict(path: str) -> dict:
+def json_to_dict(name: str) -> dict:
+    path: str = "data/" + name + "/sample.json"
     with open(path) as f:
         df: dict = json.load(f)
     return (df)
 
 
-def get_value(path: str, *args: hashable) -> Any:
+def get_value(name: str, *args: keyable) -> Any:
 
-    df: dict = json_to_dict(path)
+    df: dict = json_to_dict(name)
 
     mybox: Box = Box(df, box_dots=True)
     keys: list = list()
@@ -33,37 +36,49 @@ def get_value(path: str, *args: hashable) -> Any:
     return (mybox[KEY])
 
 
-def edit_value(table: dict, *args: hashable) -> None:
+def edit_value(name: str, key: keyable, value: Any) -> None:
+    path: str = "data/" + name + "/sample.json"
+    make_branch(name)
+    df: dict = json_to_dict(name)
 
-    key_count: int = len(args)
-    table_copy: dict = table.copy()
+    if not (key in df):
+        raise KeyError(f"'{key}'")
 
-    for i in range(key_count - 1):
-        table = {}
-
-
-def add_value(path: str, *args: dict) -> None:
-    dict_is_empty: bool
-
-    if (len(args) == 0):
-        dict_is_empty = True
-    if (len(args) == 1):
-        dict_is_empty = False
-    if (len(args) >= 2):
-        raise TypeError(
-            "add_value() takes 0 or 1 positional arguments but more arguments were given")
-
-    assert (type(args[0]) == dict)
-
+    df[key] = value
     with open(path, mode='w') as f:
-        if (dict_is_empty == True):
-            f.write(json.dumps(dict(), indent=4))
-        if (dict_is_empty == False):
-            f.write(json.dumps(args[0], indent=4))
+        json.dump(df, f, indent=4)
+
+
+def add_value(name: str, key: keyable, value: Any) -> None:
+    path: str = "data/" + name + "/sample.json"
+    make_branch(name)
+    df: dict = json_to_dict(name)
+
+    if (key in df):
+        raise KeyError(f"{key} is already exist")
+
+    df[key] = value
+    with open(path, mode='w') as f:
+        json.dump(df, f, indent=4)
 
 
 def make_branch(name: str) -> None:
-    with open(f"ponya/{name}.json", mode='w') as f:
-        f.write(json.dumps(dict(), indent=4))
+    name = str(name)
+    path: str = "data/" + name
+    if (os.path.exists(path) == False):
+        os.makedirs(path)
 
-make_branch("abcde")
+    if (os.path.exists(path + "/sample.json") == False):
+        with open(path + "/sample.json", mode='w') as f:
+            json.dump(dict(), f, indent=4)
+
+make_branch("XXX")
+add_value("XXX", "oppai", "Fcup")
+print(get_value("XXX", "oppai"))
+edit_value("XXX", "oppai", "Lcup")
+print(get_value("XXX", "oppai"))
+print(add_value("XXX", "ashi", "futomomo"))
+print(get_value("XXX"))
+edit_value("XXX", "ashi", "tsurutsuru")
+print(get_value("XXX"))
+add_value("XXX", "oppai", "Mcup")
